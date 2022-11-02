623c51a504395e106e279fd0e3367c0710bf70ef
Kodi <kodiakweb@proton.me>
2022-11-01 18:22:40 -0800

    added new zsh function

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -10,5 +10,27 @@ function chx() {
     fi
 }
 function title() {
-  echo $'\033]30;'$*$'\007'
+	echo $'\033]30;'$*$'\007'
+}
+
+function clit() {
+	local gitfolder="$HOME/.git/"
+	local url=$(xclip -o -selection clipboard)
+	local current_dir="$PWD"
+	
+	if echo "$url" | grep "https://github.com/Kodiak-Web/**"; 
+	then
+		gitfolder="$gitfolder/Kodiak-Web/"
+	fi
+	if [ ! -d "$gitfolder" ]
+	then
+    		mkdir "$gitfolder"
+	fi 
+	cd $gitfolder
+	git clone "$url"
+	cd "$current_dir"
+}
+
+function tsh {
+	tmux new-session "-As$1" "$1"
 }
